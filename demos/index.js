ac26b34374bfb92706caef18d9bb3ec2f23f0c40
Nebojša Jakovljević <nebojsa@nebjak.net>
2020-07-03 23:35:04 +0200

    improving demos

--- demos/index.js
+++ demos/index.js
@@ -1,30 +1,43 @@
 const { getCompanyDataByPIB, getCompanyDataByMB } = require('../src/index');
 
-// getCompanyDataByPIB(105359737)
-//   .then((result) => {
-//     console.dir(result);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+// Pretraga po PIB-u - Promise .then() .catch()
+getCompanyDataByPIB(105359737)
+  .then((result) => {
+    console.dir(result);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-// (async () => {
-//   try {
-//     const result = await getCompanyDataByPIB(105359737);
-//     console.dir(result);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
+// Pretragat po PIB-u - async/await
+(async () => {
+  try {
+    const result = await getCompanyDataByPIB(105359737);
+    console.dir(result);
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
-// getCompanyDataByMB('20365099')
-//   .then((result) => {
-//     console.dir(result);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+// Pretraga po PIB-u, .catch() hvata kada ne postoji rezultat pretrage
+getCompanyDataByPIB('111111111')
+  .then((result) => {
+    console.dir(result);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
+// Pretraga po maticnom broju - Promise .then() .catch()
+getCompanyDataByMB('17402200')
+  .then((result) => {
+    console.dir(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Pretraga po MB-u, .catch() hvata kada ne postoji rezultat pretrage
 getCompanyDataByMB('999')
   .then((result) => {
     console.dir(result);
